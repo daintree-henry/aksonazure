527361e3b6f8e8803166b725f3e32f3f4e3c7567
daintree-henry <daintee.henry@gmail.com>
2021-01-10 20:34:53 +0900

    update deprecated grammatic issue

--- terraform/dev/vnet/main.tf
+++ terraform/dev/vnet/main.tf
@@ -7,32 +7,32 @@ resource "azurerm_resource_group" "rg" {
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "vnet" {
     name                = "${var.prefix}vnet"
-    address_space       = ["${var.vnetAddress}"]
+    address_space       = [var.vnetAddress]
     location            = var.location
-    resource_group_name = "${azurerm_resource_group.rg.name}"
+    resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "akssubnet" {
     name                    = "${var.prefix}akssubnet"
-    resource_group_name     = "${azurerm_resource_group.rg.name}"
-    virtual_network_name    = "${azurerm_virtual_network.vnet.name}"
-    address_prefixes          = ["${var.aksSubnetAddress}"]
+    resource_group_name     = azurerm_resource_group.rg.name
+    virtual_network_name    = azurerm_virtual_network.vnet.name
+    address_prefixes          = [var.aksSubnetAddress]
 }
 resource "azurerm_subnet" "svcsubnet" {
     name                    = "${var.prefix}svcsubnet"
-    resource_group_name     = "${azurerm_resource_group.rg.name}"
-    virtual_network_name    = "${azurerm_virtual_network.vnet.name}"
-    address_prefixes          = ["${var.svcSubnetAddress}"]
+    resource_group_name     = azurerm_resource_group.rg.name
+    virtual_network_name    = azurerm_virtual_network.vnet.name
+    address_prefixes          = [var.svcSubnetAddress]
 }
 resource "azurerm_subnet" "appgwsubnet" {
     name                    = "${var.prefix}appgwsubnet"
-    resource_group_name     = "${azurerm_resource_group.rg.name}"
-    virtual_network_name    = "${azurerm_virtual_network.vnet.name}"
-    address_prefixes          = ["${var.appgwSubnetAddress}"]
+    resource_group_name     = azurerm_resource_group.rg.name
+    virtual_network_name    = azurerm_virtual_network.vnet.name
+    address_prefixes          = [var.appgwSubnetAddress]
 }
 resource "azurerm_subnet" "fwsubnet" {
     name                    = "${var.prefix}fwsubnet"
-    resource_group_name     = "${azurerm_resource_group.rg.name}"
-    virtual_network_name    = "${azurerm_virtual_network.vnet.name}"
-    address_prefixes          = ["${var.fwSubnetAddress}"]
+    resource_group_name     = azurerm_resource_group.rg.name
+    virtual_network_name    = azurerm_virtual_network.vnet.name
+    address_prefixes          = [var.fwSubnetAddress]
 }
