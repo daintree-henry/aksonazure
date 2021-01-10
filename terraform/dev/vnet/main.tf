19cd13fe6419bdccfc017507e94acc35c0f1018a
daintree-henry <daintee.henry@gmail.com>
2021-01-10 20:44:52 +0900

    20210110 add fireall

--- terraform/dev/vnet/main.tf
+++ terraform/dev/vnet/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
     name                = "${var.prefix}vnet"
     address_space       = [var.vnetAddress]
-    location            = var.location
+    location            = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
 }
 
@@ -35,4 +35,24 @@ resource "azurerm_subnet" "fwsubnet" {
     resource_group_name     = azurerm_resource_group.rg.name
     virtual_network_name    = azurerm_virtual_network.vnet.name
     address_prefixes          = [var.fwSubnetAddress]
+}
+
+resource "azurerm_public_ip" "fwpublicip" {
+  name                = "${var.prefix}fwpublicip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_firewall" "fw" {
+  name                = "${var.prefix}fw"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.fwsubnet.id
+    public_ip_address_id = azurerm_public_ip.fwpublicip.id
+  }
 }
