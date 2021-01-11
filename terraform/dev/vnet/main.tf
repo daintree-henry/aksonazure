3ffb296aaeedb06c6514147ab5587bdef2e04118
hwanghyeonwoo <dainforest9@gmail.com>
2021-01-11 11:19:12 +0900

    20210111 add firewall policy

--- terraform/dev/vnet/main.tf
+++ terraform/dev/vnet/main.tf
@@ -49,6 +49,7 @@ resource "azurerm_firewall" "fw" {
   name                = "${var.prefix}fw"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  sku_tier = "Standard"
 
   ip_configuration {
     name                 = "configuration"
@@ -56,6 +57,17 @@ resource "azurerm_firewall" "fw" {
     public_ip_address_id = azurerm_public_ip.fwpublicip.id
   }
 }
+
+resource "azurerm_firewall_policy" "fwp" {
+  name                = "${var.prefix}fwp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  dns {
+    proxy_enabled = true
+  }
+}
+
 resource "azurerm_route_table" "fwrt" {
   name                          = "${var.prefix}fwrt"
   location                      = azurerm_resource_group.rg.location
@@ -63,12 +75,24 @@ resource "azurerm_route_table" "fwrt" {
   disable_bgp_route_propagation = false
 
   route {
-    name           = "route1"
-    address_prefix = "10.1.0.0/16"
-    next_hop_type  = "vnetlocal"
+    name           = "${var.prefix}fwroute"
+    address_prefix = "0.0.0.0/0"
+    next_hop_type  = "VirtualNetworkGateway"
+    next_hop_in_ip_address = azurerm_firewall.fw.
   }
 
   tags = {
     environment = "Production"
   }
 }
+
+#az network route-table route create -g $RG --name $FWROUTE_NAME --route-table-name $FWROUTE_TABLE_NAME --address-prefix 0.0.0.0/0 --next-hop-type VirtualAppliance --next-hop-ip-address $FWPRIVATE_IP --subscription $SUBID
+# Add FW Network Rules
+
+az network firewall network-rule create -g $RG -f $FWNAME --collection-name 'aksfwnr' -n 'apiudp' --protocols 'UDP' --source-addresses '*' --destination-addresses "AzureCloud.$LOC" --destination-ports 1194 --action allow --priority 100
+az network firewall network-rule create -g $RG -f $FWNAME --collection-name 'aksfwnr' -n 'apitcp' --protocols 'TCP' --source-addresses '*' --destination-addresses "AzureCloud.$LOC" --destination-ports 9000 443
+az network firewall network-rule create -g $RG -f $FWNAME --collection-name 'aksfwnr' -n 'time' --protocols 'UDP' --source-addresses '*' --destination-fqdns 'ntp.Ubuntu.com' --destination-ports 123
+
+az network firewall network-rule create -g $RG -f $FWNAME --collection-name 'aksfwnr2' -n 'dns' --protocols 'UDP' --source-addresses '*' --destination-addresses '*' --destination-ports 53 --action allow --priority 200
+az network firewall network-rule create -g $RG -f $FWNAME --collection-name 'aksfwnr3' -n 'gitssh' --protocols 'TCP' --source-addresses '*' --destination-addresses '*' --destination-ports 22 --action allow --priority 300
+az network firewall network-rule create -g $RG -f $FWNAME --collection-name 'aksfwnr4' -n 'fileshare' --protocols 'TCP' --source-addresses '*' --destination-addresses '*' --destination-ports 445 --action allow --priority 400
