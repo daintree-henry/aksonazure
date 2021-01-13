e002b18704ed907c0e184777ed7dfb7bb3d38292
hwanghyeonwoo <dainforest9@gmail.com>
2021-01-13 16:47:25 +0900

    change rbac

--- terraform/dev/provider.tf
+++ terraform/dev/provider.tf
@@ -11,12 +11,12 @@ provider "github" {
 
 provider "kubernetes" {
   load_config_file       = "false"
-  host                   = azurerm_kubernetes_cluster.main.kube_config.0.host
-  username               = azurerm_kubernetes_cluster.main.kube_config.0.username
-  password               = azurerm_kubernetes_cluster.main.kube_config.0.password
-  client_certificate     = "${base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)}"
-  client_key             = "${base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_key)}"
-  cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)}"
+  host                   = azurerm_kubernetes_cluster.main.kube_admin_config.0.host
+  username               = azurerm_kubernetes_cluster.main.kube_admin_config.0.username
+  password               = azurerm_kubernetes_cluster.main.kube_admin_config.0.password
+  client_certificate     = "${base64decode(azurerm_kubernetes_cluster.main.kube_admin_config.0.client_certificate)}"
+  client_key             = "${base64decode(azurerm_kubernetes_cluster.main.kube_admin_config.0.client_key)}"
+  cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.main.kube_admin_config.0.cluster_ca_certificate)}"
   version = ">=1.13.3"
 }
 
@@ -26,11 +26,11 @@ provider "tls" {
 
 provider "helm" {
   kubernetes {
-    host                   = azurerm_kubernetes_cluster.main.kube_config.0.host
-    username               = azurerm_kubernetes_cluster.main.kube_config.0.username
-    password               = azurerm_kubernetes_cluster.main.kube_config.0.password
-    client_certificate     = "${base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)}"
-    client_key             = "${base64decode(azurerm_kubernetes_cluster.main.kube_config.0.client_key)}"
-    cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)}"
+    host                   = azurerm_kubernetes_cluster.main.kube_admin_config.0.host
+    username               = azurerm_kubernetes_cluster.main.kube_admin_config.0.username
+    password               = azurerm_kubernetes_cluster.main.kube_admin_config.0.password
+    client_certificate     = "${base64decode(azurerm_kubernetes_cluster.main.kube_admin_config.0.client_certificate)}"
+    client_key             = "${base64decode(azurerm_kubernetes_cluster.main.kube_admin_config.0.client_key)}"
+    cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.main.kube_admin_config.0.cluster_ca_certificate)}"
   }
 }
